1fde7fc96d8bb11cac3dff88efef3f4d6e97eb86
Felecia Davis <fmdavis1@gmail.com>
2022-05-31 15:27:09 -0400

    Added dynamic anchor tags to index.ejs.

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,6 +1,4 @@
-const { builtinModules } = require("module")
-
-pokemon = [
+const pokemon = [
     {name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur"},
     {name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur"},
     {name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur"},
--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/pokemon', (req, res) => {
 })
 
 app.get('/pokemon/:id', (req, res) => {
-    res.send(req.params.id)
+    res.render('show', {pokemon:pokemon, index:Number(req.params.id)})
 })
 
 app.listen(PORT, () => {
