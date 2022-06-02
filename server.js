17120b765946d4e9a1a681eca19d54bff1d57048
Felecia Davis <fmdavis1@gmail.com>
2022-06-02 16:25:03 -0400

    added create new pokemon page and set up database.

--- server.js
+++ server.js
@@ -2,28 +2,107 @@
 const express = require('express')
 
 const pokemon = require('./models/pokemon')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const PokemonModel = require('./models/PokemonModel')
+
 
 //createa an instance of express
 const app = express()
 const PORT = 3000
 
+//Middleware functions
+//They update the request as soon as they come in.
+
+// app.use((req, res, next) => {
+//     console.log('Running the middleware function!')
+//     next() //Go to the next middleware or the response.
+// })
+
+//JSON Middleware
+app.use(express.json())
+
+//if we don't need to read data from the url
+app.use(express.urlencoded({extended: false}))
+
 //Set up view engine
 app.set('view engine', 'ejs')
 app.set('views', './views')
 
-//root route
+//*===============ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
 
 app.get('/pokemon', (req, res) => {
-    res.render('index', {data: pokemon})
+    res.render('index', {
+        pageTitle: 'Pokemon',
+        pageHeader: 'See All The Pokemon!',
+        pokemon: pokemon
+    })
 })
 
-app.get('/pokemon/:id', (req, res) => {
-    res.render('show', {pokemon:pokemon, index:Number(req.params.id)})
+app.get('/pokemon/new', (req, res) => {
+    res.render('new-pokemon', {
+        pageTitle: 'New Pokemon',
+        pageHeader: " Create a new Pokemon ",
+       // index:Number(req.params.id),
+        //pokemon:pokemon,pokemon:pokemon, index:Number(req.params.id)})
 })
+
+})
+
+app.post('/pokemon', async(req, res) => {
+        const newPokemon = req.body//create newPokemon variable
+        //add a img property to the object
+        console.log('newPokemon=',newPokemon)
+        newPokemon.img = `http://img.pokemondb.net/artwork/${req.body.name}`
+       console.log(newPokemon)
+        // pokemon.push(req.body)
+        // res.redirect('/pokemon')
+        await PokemonModel.create(newPokemon, ( error, result) =>{
+            if (error){
+                console.log(error)
+            }
+            console.log(result)
+        })
+        res.send('done')
+    })
+
+app.get('/pokemon/:id', (req, res) => {
+    // res.send(req.params.id)
+
+    res.render('show', {
+        pageTitle: 'Details',
+        pageHeader: " Gotta Catch 'Em All ",
+        index:Number(req.params.id),
+        pokemon:pokemon,
+        // index:pokemon[req.params.id]
+        
+        // pokemon: pokemon[req.params.id]
+    })
+})
+// app.get('/pokemon', (req, res) => {
+//     res.render('index', {data: pokemon})
+// })
+
+// app.get('/pokemon/:id', (req, res) => {
+//     res.render('show', {pokemon:pokemon, index:Number(req.params.id)})
+// })
+
+
+
+// app.post('/pokemon',(req, res) => {
+//     console.log(req.body)
+//     pokemon.push(req.body)
+//     res.redirect('/pokemon')
+
+// })
+
+//*============LISTENER
 
 app.listen(PORT, () => {
     console.log('Server is running...')
+    mongoose.connect(process.env.MONGODB_URI)//CONNECTS TO MONGO DB
+    console.log('MongoDB connected!')
 })
